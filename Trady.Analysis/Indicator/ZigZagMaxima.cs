7ec11e4f26b93fa7e7cab4e43defe2e1f8926c09
richard sjöber <richard.sjoberg@live.com>
2017-11-16 00:25:36 +0100

    fix support/resistance calculation index

--- Trady.Analysis/Indicator/ZigZagMaxima.cs
+++ Trady.Analysis/Indicator/ZigZagMaxima.cs
@@ -41,7 +41,7 @@ namespace Trady.Analysis.Indicator
             var previousLow = historicalCloses.LastOrDefault(c => currentClose > c && (currentClose - c) / currentClose > _threshold);
             if (futureLow.HasValue && previousLow.HasValue)
             {
-                return (currentClose, futureCloses.IndexOf(futureLow));
+                return (currentClose, index + futureCloses.FindIndex(c => (currentClose - c) / currentClose > _threshold) + 1);
             }
             return null;
         }
