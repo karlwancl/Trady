a1704172f1a928109b4734de2febfaf3690ad1da
irperez <irperez@gmail.com>
2017-11-15 23:05:12 -0500

    Added import configuration class Added CSV import flexibility (can be extended to add more options) Added accompanied tests

--- Trady.Importer.Csv/CsvImporter.cs
+++ Trady.Importer.Csv/CsvImporter.cs
@@ -12,6 +12,7 @@ using CsvHelper.Configuration;
 using Trady.Core;
 using Trady.Core.Infrastructure;
 using Trady.Core.Period;
+using CsvHelper.TypeConversion;
 
 namespace Trady.Importer.Csv
 {
@@ -19,28 +20,39 @@ namespace Trady.Importer.Csv
     {
         private string _path;
         private readonly CultureInfo _culture;
+        private string _format;
+        private string _delimiter;
+        private bool _hasHeader = true;
 
         public CsvImporter(string path) : this(path, CultureInfo.CurrentCulture)
-        {
+        {            
         }
 
         public CsvImporter(string path, CultureInfo culture)
         {
             _path = path;
             _culture = culture;
+
+        }
+
+        public CsvImporter(string path, CsvImportConfiguration configuration): this(path, configuration.Culture)
+        {
+            _format = configuration.DateFormat;
+            _delimiter = configuration.Delimiter;
+            _hasHeader = configuration.HasHeaderRecord;
         }
 
         public async Task<IReadOnlyList<IOhlcvData>> ImportAsync(string symbol, DateTime? startTime = null, DateTime? endTime = null, PeriodOption period = PeriodOption.Daily, CancellationToken token = default(CancellationToken))
             => await Task.Factory.StartNew(() =>
-            {
+            {               
                 using (var fs = File.OpenRead(_path))
                 using (var sr = new StreamReader(fs))
-                using (var csvReader = new CsvReader(sr, new CsvConfiguration() { CultureInfo = _culture }))
+                using (var csvReader = new CsvReader(sr, new CsvConfiguration() { CultureInfo = _culture, Delimiter = string.IsNullOrWhiteSpace(_delimiter) ? "," : _delimiter, HasHeaderRecord = _hasHeader }))
                 {
                     var candles = new List<IOhlcvData>();
                     while (csvReader.Read())
                     {
-                        var date = csvReader.GetField<DateTime>(0);
+                        var date = string.IsNullOrWhiteSpace(_format) ? csvReader.GetField<DateTime>(0) : DateTime.ParseExact(csvReader.GetField<string>(0), _format, _culture);                        
                         if ((!startTime.HasValue || date >= startTime) && (!endTime.HasValue || date <= endTime))
                             candles.Add(GetRecord(csvReader));
                     }
@@ -52,7 +64,7 @@ namespace Trady.Importer.Csv
         {
             // By using GetField Methodo of the CSV Reader Culture Info set in the configuration is used
             return new Candle(
-                csv.GetField<DateTime>(0),
+                string.IsNullOrWhiteSpace(_format) ? csv.GetField<DateTime>(0) : DateTime.ParseExact(csv.GetField<string>(0), _format, _culture),
                 csv.GetField<Decimal>(1),
                 csv.GetField<Decimal>(2),
                 csv.GetField<Decimal>(3),
