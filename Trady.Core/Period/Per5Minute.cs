425dee5d6ff4538d986ade9262e37e538a31bda9
Karl Wan <lppkarl@gmail.com>
2017-12-30 11:43:05 +0800

    Added Per5Minute & Per10Minute PeriodOption

---
+++ Trady.Core/Period/Per5Minute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Trady.Core.Period
+{
+    public class Per5Minute : IntradayPeriodBase
+    {
+        public override uint NumberOfSecond => 5 * 60;
+
+        public override bool IsTimestamp(DateTimeOffset dateTime)
+            => dateTime.Minute % 5 == 0 && dateTime.Second == 0 && dateTime.Millisecond == 0;
+    }
+}
