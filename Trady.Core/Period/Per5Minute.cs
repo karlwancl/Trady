f72a319c80b2300a08058ecc88ab26745af198a4
Karl Wan <lppkarl@gmail.com>
2017-12-30 11:51:44 +0800

    Fixes #22 @239fca4

--- Trady.Core/Period/Per5Minute.cs
+++ Trady.Core/Period/Per5Minute.cs
@@ -3,7 +3,7 @@ namespace Trady.Core.Period
 {
     public class Per5Minute : IntradayPeriodBase
     {
-        public override uint NumberOfSecond => 5 * 60;
+        public override uint NumberOfSecond => 5 * 60; 
 
         public override bool IsTimestamp(DateTimeOffset dateTime)
             => dateTime.Minute % 5 == 0 && dateTime.Second == 0 && dateTime.Millisecond == 0;
