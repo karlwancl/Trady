7d7a97da831276a5661d9986f8e2ebfa721031f5
Karl Wan <lppkarl@gmail.com>
2018-03-26 15:23:03 +0800

    Added test case for #44

---
+++ Trady.Analysis/Indicator/ZigZag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Trady.Analysis.Infrastructure;
+
+namespace Trady.Analysis.Indicator
+{
+    public class ZigZag<TInput, TOutput> : AnalyzableBase<TInput, decimal, decimal?, TOutput>
+    {
+        readonly decimal threshold;
+
+        protected ZigZag(IEnumerable<TInput> inputs, Func<TInput, decimal> inputMapper, decimal threshold = 0.03m) : base(inputs, inputMapper)
+        {
+            this.threshold = threshold;
+        }
+
+        protected override decimal? ComputeByIndexImpl(IReadOnlyList<decimal> mappedInputs, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
