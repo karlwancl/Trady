f37364a7de66693f2017cf8f5b3005398329acb1
Karl Wan <karl.chun.lun.wan@avanade.com>
2018-07-08 18:09:46 +0800

    Use local time for yahoo finance query

--- Trady.Importer.Yahoo/YahooFinanceImporter.cs
+++ Trady.Importer.Yahoo/YahooFinanceImporter.cs
@@ -16,6 +16,10 @@ namespace Trady.Importer.Yahoo
         private static readonly DateTime UnixMinDateTime = new DateTime(1901, 12, 13);
         private static readonly DateTime UnixMaxDateTime = new DateTime(2038, 1, 19);
 
+        private static readonly TimeZoneInfo TzEst = TimeZoneInfo
+            .GetSystemTimeZones()
+            .Single(tz => tz.Id == "Eastern Standard Time" || tz.Id == "America/New_York");
+
         private static readonly IDictionary<PeriodOption, Period> PeriodMap = new Dictionary<PeriodOption, Period>
         {
             {PeriodOption.Daily, Period.Daily },
@@ -37,29 +41,32 @@ namespace Trady.Importer.Yahoo
             if (period != PeriodOption.Daily && period != PeriodOption.Weekly && period != PeriodOption.Monthly)
                 throw new ArgumentException("This importer only supports daily, weekly & monthly data");
 
-            var corrStartTime = (startTime < UnixMinDateTime ? UnixMinDateTime : startTime) ?? UnixMinDateTime;
-            var corrEndTime = AddPeriod((endTime > UnixMaxDateTime ? UnixMaxDateTime : endTime) ?? UnixMaxDateTime, period);
-            var candles = await YahooFinanceApi.Yahoo.GetHistoricalAsync(symbol, corrStartTime, corrEndTime, PeriodMap[period], false, false, token);
+            var corrStartTime = ConvertLocalTimeToEst((startTime < UnixMinDateTime ? UnixMinDateTime : startTime) ?? UnixMinDateTime);
+            var corrEndTime = ConvertLocalTimeToEst((endTime > UnixMaxDateTime ? UnixMaxDateTime : endTime) ?? UnixMaxDateTime);
+            var candles = await YahooFinanceApi.Yahoo.GetHistoricalAsync(symbol, corrStartTime, corrEndTime, PeriodMap[period], token);
 
             return candles.Select(c => new Core.Candle(c.DateTime, c.Open, c.High, c.Low, c.Close, c.Volume)).OrderBy(c => c.DateTime).ToList();
         }
 
-        private static DateTime AddPeriod(DateTime dateTime, PeriodOption period)
-        {
-            switch (period)
-            {
-                case PeriodOption.Daily:
-                    return dateTime.AddDays(1);
+        private DateTime ConvertLocalTimeToEst(DateTime dateTime)
+            => TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.Local, TzEst);
 
-                case PeriodOption.Weekly:
-                    return dateTime.AddDays(7);
+        //private static DateTime AddPeriod(DateTime dateTime, PeriodOption period)
+        //{
+        //    switch (period)
+        //    {
+        //        case PeriodOption.Daily:
+        //            return dateTime.AddDays(1);
 
-                case PeriodOption.Monthly:
-                    return dateTime.AddMonths(1);
+        //        case PeriodOption.Weekly:
+        //            return dateTime.AddDays(7);
 
-                default:
-                    return dateTime;
-            }
-        }
+        //        case PeriodOption.Monthly:
+        //            return dateTime.AddMonths(1);
+
+        //        default:
+        //            return dateTime;
+        //    }
+        //}
     }
 }
